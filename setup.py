6963cd670465c21ca58b8abcfeec9e9cafad1eb9
j0ono0 <john@johnnewall.com>
2021-03-10 16:43:57 +1030

    add templates files to setup.py

--- setup.py
+++ setup.py
@@ -24,4 +24,8 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     python_requires=">=3.6",
     install_requires=["Jinja2>=2.11.3"],
+    data_files=[
+        ('templates',['groups.svg','image.svg','pin_label.svg','svg.svg']),
+        ('templates/styles', ['pin_label_styles.css'])
+    ]
 )

f63d73c5a2568668c80e66d3419b182a721c707e
j0ono0 <john@johnnewall.com>
2021-03-10 22:02:12 +1030

    update version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pinout",
-    version="0.0.1",
+    version="0.0.2",
     author="John Newall",
     author_email="john@johnnewall.com",
     description="Generate graphical pinout references for electronic hardware.",

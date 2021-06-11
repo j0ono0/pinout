0dddf0e9f253c0f6e6ab8128cdfb3d33163289b7
j0ono0 <john@johnnewall.com>
2021-06-11 15:50:01 +0930

    update quick start sample

---
+++ pinout/resources/quick_start/data.py
@@ -0,0 +1,67 @@
+legend = [
+    ("Analog", "analog"),
+    ("Communication", "comms"),
+    ("Ground", "gnd"),
+    ("GPIO", "gpio"),
+    ("Touch", "touch"),
+    ("Power", "pwr"),
+    ("PWM", "pwm"),
+]
+
+# Pinlabels
+
+left_header = [
+    [
+        ("0", "gpio"),
+        ("A0", "analog"),
+        ("MISO", "comms"),
+    ],
+    [
+        ("1", "gpio"),
+        ("MOSI", "comms", {"body": {"x": 92}}),
+    ],
+    [
+        ("2", "gpio"),
+        ("A1", "analog"),
+        ("SCLK", "comms"),
+    ],
+]
+
+lower_header = [
+    [
+        ("3", "gpio"),
+        ("PWM", "pwm"),
+    ],
+    [
+        ("4", "gpio"),
+        ("A2", "analog"),
+        ("TOUCH", "touch"),
+    ],
+    [
+        ("5", "gpio"),
+        ("A3", "analog"),
+    ],
+]
+
+right_header = [
+    [
+        ("Vcc", "pwr"),
+    ],
+    [
+        ("GND", "gnd"),
+    ],
+    [
+        ("6", "gpio"),
+        ("A4", "analog"),
+        ("TOUCH", "touch"),
+    ],
+]
+
+
+# Text
+
+title = ["<tspan class='h1'>Pinout Quick start</tspan>"]
+
+description = """Python tool kit to assist with 
+documentation of electronic hardware. 
+More info at <tspan class='italic strong'>pinout.readthedocs.io</tspan>"""
--- pinout/resources/quick_start/quick_start_pinout.py
+++ pinout/resources/quick_start/quick_start_pinout.py
@@ -1,64 +1,128 @@
-from pinout import layout
+###########################################
+#
+# Example script to build a
+# pinout diagram. Includes basic
+# features and convenience classes.
+#
+###########################################
 
-# Custom pinlabel configuration
-long_label_config = {"label": {"rect": {"width": 108}}}
+from pinout.core import Diagram, Group, Rect, Image
+from pinout.components.pinlabel import PinLabelGroup, Label
+from pinout.components.type import TextBlock
+from pinout.components import leaderline as lline
+from pinout.components.legend import Legend
 
-# Pin label information grouped into dicts that render sets of labels
-pin_headers = [
-    {
-        # LHS header
-        "x": 16,
-        "y": 100,
-        "pitch": (0, 30),
-        "offset": (-58, 0),
-        "labels": [
-            [("Vcc", "pwr", long_label_config)],
-            [("1", "gpio"), ("A1", "analog")],
-            [("2", "gpio"), ("PWM", "pwm", {"offset": (66, 0)})],
-        ],
-    },
-    {
-        # RHS header
-        "x": 204,
-        "y": 100,
-        "pitch": (0, 30),
-        "offset": (58, 0),
-        "labels": [
-            [("GND", "pwr", long_label_config)],
-            [("7", "gpio"), ("A3", "analog")],
-            [("6", "gpio"), ("A2", "analog"), ("PWM", "pwm")],
-        ],
-    },
-    {
-        # Lower header
-        "x": 65,
-        "y": 244,
-        "pitch": (30, 0),
-        "offset": (-107, 40),
-        "labels": [
-            [("RST", "pwr", long_label_config)],
-            [("3", "gpio"), ("ADC0", "analog")],
-            [("4", "gpio"), ("ADC1", "analog"), ("PWM", "pwm")],
-            [("5", "gpio"), ("PWM", "pwm", {"offset": (66, 0)})],
-        ],
-    },
-]
+import data
 
-# Create a new diagram
-diagram = layout.Diagram()
+# Create a new diagram and add a background
+diagram = Diagram(896, 504, "diagram")
+diagram.add(Rect(0, 0, 896, 504, "diagram__bg"))
 
-# Override default config with user supplied values
-diagram.add_config("quick_start_config.yaml")
+# Add a stylesheet
+diagram.add_stylesheet("styles.css", True)
 
-# Add an image
-diagram.add_image("quick_start_hardware.png", width=220, height=260, embed=True)
+# Create a 'main' panel and add a background
+panel_main = diagram.add(Group(2, 2, "panel panel--main"))
+panel_main.add(Rect(0, 0, 892, 500, "panel__bg"))
 
-# Add a legend. Note, categories are documented in config.yaml
-diagram.add_legend(x=250, y=200)
+# Create a group to hold the pinout-diagram components.
+graphic = panel_main.add(Group(338, 8))
 
-# Create pinout labels
-for header in pin_headers:
-    diagram.add_pinlabelset(**header)
+# Add and embed an image
+graphic.add(Image("quick_start_hardware.png", width=220, height=260, embed=True))
 
-# Export the finished diagram
-diagram.export("quick_start_diagram.svg", overwrite=True)
+
+# Example: adding a single pin label
+graphic.add(
+    Label(
+        content="RESET",
+        x=155,
+        y=244,
+        tag="pwr",
+        body={"x": 117, "y": 30},
+        leaderline={"direction": "vh"},
+    )
+)
+
+
+# Add a pinlabels to the right header
+graphic.add(
+    PinLabelGroup(
+        x=206,
+        y=100,
+        pin_pitch=(0, 30),
+        label_start=(60, 0),
+        label_pitch=(0, 0),
+        labels=data.right_header,
+    )
+)
+
+# Add a pinlabels to the left header
+graphic.add(
+    PinLabelGroup(
+        x=16,
+        y=100,
+        pin_pitch=(0, 30),
+        label_start=(60, 0),
+        label_pitch=(0, 0),
+        scale=(-1, 1),
+        labels=data.left_header,
+    )
+)
+
+
+graphic.add(
+    PinLabelGroup(
+        x=65,
+        y=244,
+        scale=(-1, 1),
+        pin_pitch=(30, 0),
+        label_start=(110, 30),
+        label_pitch=(30, 30),
+        labels=data.lower_header,
+        leaderline=lline.Curved(direction="vh"),
+    )
+)
+
+info_panel = diagram.add(Group(x=2, y=368, tag="panel panel--info"))
+info_panel.add(Rect(0, 0, 892, 132, tag="panel__bg"))
+
+
+title_block = info_panel.add(
+    TextBlock(
+        data.title,
+        x=0,
+        y=0,
+        width=338,
+        height=42,
+        offset=(20, 33),
+        line_height=18,
+        tag="panel title_block",
+    )
+)
+
+legend = info_panel.add(
+    Legend(
+        data.legend,
+        x=338,
+        y=0,
+        max_height=132,
+    )
+)
+
+
+info_panel.add(
+    TextBlock(
+        data.description.split("\n"),
+        x=0,
+        y=title_block.y + title_block.height,
+        width=title_block.width,
+        height=info_panel.height - title_block.height,
+        offset=(20, 18),
+        line_height=18,
+        tag="panel text_block",
+    )
+)
+
+# Export final SVG diagram
+diagram.export("pinout_diagram.svg", True)

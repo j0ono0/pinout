83c2d1258ff23acf53f182fcdda7b6be99190641
j0ono0 <john@johnnewall.com>
2021-06-20 20:10:36 +0930

    wip: compact teensy4.0 version

---
+++ samples/teensy/teensy_4.0_compact/data.py
@@ -0,0 +1,144 @@
+lline_84 = {"body": {"x": 84}, "tag": "lline--visible"}
+lline_53 = {"body": {"x": 53}, "tag": "lline--visible"}
+lline_40 = {"body": {"x": 40}, "tag": "lline--visible"}
+lg_body = {"body": {"width": 170}}
+
+
+# Pinlabels
+
+header_rhs = [
+    [("Vin (3.6 to 5.5 volts)", "pwr", lg_body)],
+    [("GND", "gnd", lg_body)],
+    [("3.3V (250 mA max)", "pwr", lg_body)],
+    [
+        ("23", "digital"),
+        ("A9", "analog"),
+        ("PWM", "pwm"),
+        ("CRX1", "canbus"),
+        ("MCLK1", "audio"),
+    ],
+    [
+        ("22", "digital"),
+        ("A8", "analog"),
+        ("PWM", "pwm"),
+        ("CTX1", "canbus"),
+        ("MCLK1", "audio"),
+    ],
+    [
+        ("21", "digital"),
+        ("A7", "analog"),
+        ("RX5", "serial", lline_53),
+        ("BCLK1", "audio"),
+    ],
+    [
+        ("20", "digital"),
+        ("A6", "analog"),
+        ("TX5", "serial", lline_53),
+        ("LRCLK1", "audio"),
+    ],
+    [("19", "digital"), ("A5", "analog"), ("PWM", "pwm"), ("SCL0", "i2c", lline_84)],
+    [("18", "digital"), ("A4", "analog"), ("PWM", "pwm"), ("SDA0", "i2c", lline_84)],
+    [("17", "digital"), ("A3", "analog"), ("TX4", "serial", lline_53), ("SDA1", "i2c")],
+    [("16", "digital"), ("A2", "analog"), ("RX4", "serial", lline_53), ("SCL1", "i2c")],
+    [
+        ("15", "digital"),
+        ("A1", "analog"),
+        ("PWM", "pwm"),
+        ("RX3", "serial"),
+        ("S/PDIF IN", "audio tight"),
+    ],
+    [
+        ("14", "digital"),
+        ("A0", "analog"),
+        ("PWM", "pwm"),
+        ("TX3", "serial"),
+        ("S/PDIF OUT", "audio tight"),
+    ],
+    [
+        ("13", "digital"),
+        ("PWM", "pwm", lline_40),
+        ("SCK", "spi"),
+        ("CRX1", "canbus"),
+    ],
+]
+
+header_lhs = [
+    [("GND", "gnd", lg_body)],
+    [("0", "digital"), ("PWM", "pwm"), ("RX1", "serial"), ("CRX2", "canbus")],
+    [("1", "digital"), ("PWM", "pwm"), ("TX1", "serial"), ("CTX2", "canbus")],
+    [("2", "digital"), ("PWM", "pwm"), ("OUT2", "audio", lline_84)],
+    [("3", "digital"), ("PWM", "pwm"), ("LRCLK2", "audio", lline_84)],
+    [("4", "digital"), ("PWM", "pwm"), ("BCLK2", "audio", lline_84)],
+    [("5", "digital"), ("PWM", "pwm"), ("IN2", "audio", lline_84)],
+    [("6", "digital"), ("PWM", "pwm"), ("OUT1D", "audio", lline_84)],
+    [("7", "digital"), ("PWM", "pwm"), ("RX2", "serial"), ("OUT1A", "audio")],
+    [("8", "digital"), ("PWM", "pwm"), ("TX2", "serial"), ("IN1", "audio")],
+    [("9", "digital"), ("PWM", "pwm"), ("OUT1C", "audio", lline_84)],
+    [("10", "digital"), ("PWM", "pwm"), ("CS", "spi"), ("MQSR", "audio")],
+    [("11", "digital"), ("PWM", "pwm"), ("MOSI", "spi"), ("CTX1", "canbus")],
+    [("12", "digital"), ("PWM", "pwm"), ("MISO", "spi"), ("MQSL", "audio")],
+]
+
+
+header_end_lhs = [
+    [("VBat", "pwr")],
+    [("3.3v", "pwr")],
+    [("GND", "gnd")],
+]
+header_end_rhs = [
+    [("Program", "pwr")],
+    [("On/Off", "gnd")],
+]
+
+
+# Legend
+legend_digital = """<tspan class='legend__title'>Digital Pins</tspan>
+digitalRead
+digitalWrite
+pinMode"""
+
+legend_analog = """<tspan class='legend__title'>Analog Pins</tspan>
+analogRead"""
+
+legend_pwm = """<tspan class='legend__title'>PWM Pins</tspan>
+analogWrite"""
+
+legend_audio = """<tspan class='legend__title'>Digital Audio</tspan>
+Audio library"""
+
+legend_serial = """<tspan class='legend__title'>Serial Ports</tspan>
+Serial1 - Serial7"""
+
+legend_i2c = """<tspan class='legend__title'>I2C Port</tspan>
+Wire library"""
+
+legend_spi = """<tspan class='legend__title'>SPI Port</tspan>
+SPI library"""
+
+legend_canbus = """<tspan class='legend__title'>CAN Bus</tspan>
+FlexCAN_t4 library"""
+
+legend_content = [
+    (legend_digital, "digital"),
+    (legend_analog, "analog"),
+    (legend_pwm, "pwm"),
+    (legend_audio, "audio"),
+    (legend_serial, "serial"),
+    (legend_i2c, "i2c"),
+    (legend_spi, "spi"),
+    (legend_canbus, "canbus"),
+]
+
+# Text
+
+title = ["Teensy 4 pinout"]
+
+title_2 = """32 Bit Arduino Compatible 
+Microcontroller"""
+
+instructions = """To begin using Teensy please visit 
+'Getting Started' at <tspan class='url'>www.pjrc.com/teensy</tspan>"""
+
+notes = """&#8226; All digital pins have interrupt capability.
+&#8226; Loading status (Red LED): dim: Ready  |  bright: Writing  |  blink: No USB
+&#8226; LED on pin-13"""
---
+++ samples/teensy/teensy_4.0_compact/pinout_diagram.py
@@ -0,0 +1,208 @@
+# Teensy 4.0 Pinout
+# https://www.pjrc.com/store/teensy40.html
+
+from pinout.core import Diagram, Rect, Group, Image
+from pinout import config
+from pinout.components.pinlabel import PinLabelGroup
+from pinout.components.type import TextBlock
+from pinout.components import leaderline as lline
+
+import data
+
+# Override default config settings
+config.pinlabel["body"]["x"] = 2
+
+################################################
+# EXPERIMENTAL: DATA PREPROCESSOR
+# modify data based on its tag name
+# to further minimise config in data
+
+
+def update_config(source, update):
+    for k, v in update.items():
+        if type(v) is dict:
+            source[k] = source.get(k, {})
+            update_config(source[k], v)
+        else:
+            source[k] = v
+    return source
+
+
+def data_preprocessor(data_list):
+    for row in data_list:
+        for i, label in enumerate(row):
+            try:
+                data_config = label[2]
+            except IndexError:
+                data_config = {}
+
+            config = {}
+            if label[1] in ["digital", "analog"]:
+                config = {"body": {"width": 36}}
+            elif label[1] in ["pwm"]:
+                config = {"body": {"width": 49}}
+
+            update_config(config, data_config)
+
+            row[i] = (label[0], label[1], config)
+        yield row
+
+
+################################################
+
+
+# Create a new diagram and add a background
+# Official Teensy pinout cards are 1.41 ratio (or very close)
+# and comfortable to read at approx A6 dimensions 148mm x 105mm
+# 10 "units" is equal to 1mm if printed at A6
+diagram = Diagram(1128, 800, "diagram")
+
+diagram.add(Rect(0, 0, 1128, 800, "diagram__bg"))
+
+# Add a stylesheet
+diagram.add_stylesheet("styles.css", True)
+
+# Create a layout
+panel_main = diagram.add(Group(10, 10, "panel panel--main"))
+panel_main.add(Rect(0, 0, 1108, 638, "panel__bg"))
+
+
+# Create a group to hold the actual diagram components.
+graphic = panel_main.add(Group(340, 25, scale=(1.1, 1.1)))
+
+# Custom legend
+legend = diagram.add(Group(971, 10, "legend"))
+legend.add(Rect(0, 0, 147, 780, "legend__bg panel__bg"))
+
+# Title bar
+titlebar = diagram.add(Group(10, 650, "titlebar"))
+titlebar.add(Rect(0, 0, 961, 140, "titlebar__bg panel__bg"))
+
+# Add and embed an image
+graphic.add(
+    Image(
+        "hardware_teensy_4.0_front.svg",
+        x=-8.5,
+        y=-7.5,
+        width=210,
+        height=340,
+        embed=True,
+    )
+)
+
+# Pinlabels: Right header
+graphic.add(
+    PinLabelGroup(
+        x=195,
+        y=32,
+        pin_pitch=(0, 30),
+        label_start=(40, 0),
+        label_pitch=(0, 30),
+        labels=data_preprocessor(data.header_rhs),
+    )
+)
+
+# Pinlabels: Left header
+graphic.add(
+    PinLabelGroup(
+        x=15,
+        y=32,
+        scale=(-1, 1),
+        pin_pitch=(0, 30),
+        label_start=(40, 0),
+        label_pitch=(0, 30),
+        labels=data_preprocessor(data.header_lhs),
+    )
+)
+
+# Pinlabels: End, left side
+graphic.add(
+    PinLabelGroup(
+        x=45,
+        y=422,
+        scale=(-1, 1),
+        pin_pitch=(30, 0),
+        label_start=(70, 30),
+        label_pitch=(0, 30),
+        labels=data.header_end_lhs,
+        leaderline=lline.Curved(direction="vh"),
+        body={"width": 166},
+    )
+)
+
+# Pinlabels: End, right side
+graphic.add(
+    PinLabelGroup(
+        x=135,
+        y=422,
+        pin_pitch=(30, 0),
+        label_start=(100, 90),
+        label_pitch=(0, -30),
+        labels=data.header_end_rhs,
+        leaderline=lline.Curved(direction="vh"),
+        body={"width": 166},
+    )
+)
+
+# Legend entries
+gutter = 2
+entry_height = 95.75
+for ind, (entry, tag) in enumerate(data.legend_content):
+
+    legend.add(
+        TextBlock(
+            entry.split("\n"),
+            x=gutter,
+            y=0 + ind * (entry_height + gutter),
+            width=145,
+            height=entry_height,
+            offset=(10, 28),
+            line_height=18,
+            tag=f"{tag} legend__entry",
+        )
+    )
+
+titlebar.add(
+    TextBlock(
+        data.title,
+        x=20,
+        y=38,
+        offset=(0, 0),
+        line_height=16,
+        tag="h1",
+    )
+)
+titlebar.add(
+    TextBlock(
+        data.title_2.split("\n"),
+        x=20,
+        y=48,
+        offset=(8, 18),
+        line_height=18,
+        tag="h2",
+    )
+)
+titlebar.add(
+    TextBlock(
+        data.instructions.split("\n"),
+        x=20,
+        y=90,
+        offset=(8, 18),
+        line_height=18,
+        tag="p",
+    )
+)
+titlebar.add(
+    TextBlock(
+        data.notes.split("\n"),
+        x=395,
+        y=72,
+        offset=(8, 18),
+        line_height=18,
+        tag="p",
+    )
+)
+
+
+# Export final SVG diagram
+diagram.export("teensy_4.0_front_pinout_diagram.svg", True)

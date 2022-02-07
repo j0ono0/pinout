8a718a4699468d93175624f0683d2dc9a628de78
John Newall <john@johnnewall.com>
2022-02-08 10:05:45 +1030

    update version

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2021, John Newall"
 author = "John Newall"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.19"
+release = "0.0.20"
 
 
 # -- General configuration ---------------------------------------------------
